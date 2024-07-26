7960cf0e7c4b5a7672cdf653c3f34ea1d946e5c5
Purandhar Kola <purandhar.kola@cognine.com>
2024-07-26 17:15:52 +0530

    Commit

---
+++ greatestproduct/Calculate.cs
@@ -0,0 +1,19 @@
+﻿namespace Calculate
+{
+    class Calculateclass
+    {
+        public static int calculate(string inputString)
+        {
+            int product = 1;
+            foreach (char character in inputString)
+            {
+                //int digit = c - '0';
+                //product*=digit;
+                product *= (int)Char.GetNumericValue(character);
+
+            }
+            return product;
+        }
+    }
+
+}
